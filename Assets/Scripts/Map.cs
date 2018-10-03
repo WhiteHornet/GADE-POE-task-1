4df3bb2d23a5d9d64a2795dd5f79e40b75a64f34
Pieter Joubert <pjoubert@vegaschool.com>
2018-10-03 13:23:04 +0200

    Basic prototype created

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject oceanTile;
+    public int size;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int x = 0; x < size; x++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                Instantiate(oceanTile, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+		
+	}
+}
