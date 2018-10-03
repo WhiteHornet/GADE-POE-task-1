ea6243b146659fed4df3989dd24f7b11f4445813
Pieter Joubert <pjoubert@vegaschool.com>
2018-10-03 14:25:43 +0200

    Map class, Tile class added. Create and Display Map methods added

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,22 +5,64 @@ using UnityEngine;
 public class Map : MonoBehaviour {
 
     public GameObject oceanTile;
+    public GameObject hitTile;
+    public GameObject missTile;
     public int size;
+    private Tile[,] map;
 
 	// Use this for initialization
 	void Start () {
+
+        CreateMap();
+        DisplayMap();
 		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        
+	}
+
+    void CreateMap()
+    {
+        map = new Tile[size, size];
         for (int x = 0; x < size; x++)
         {
             for (int z = 0; z < size; z++)
             {
-                Instantiate(oceanTile, new Vector3(x, 0, z), Quaternion.identity);
+                Tile t = new Tile();
+                if (Random.Range(0, 100) > 80)
+                {
+                    t.TileType = TileType.Hit;
+                }
+                else
+                {
+                    t.TileType = TileType.Ocean;
+                }
+                map[x, z] = t;
             }
         }
-		
-	}
+    }
+
+    void DisplayMap()
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                switch (map[x, z].TileType)
+                {
+                    case TileType.Ocean:
+                        Instantiate(oceanTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                    case TileType.Hit:
+                        Instantiate(hitTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                    case TileType.Miss:
+                        Instantiate(missTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TileType { Ocean, Hit, Miss}
+
+public class Tile 
+{
+    public TileType TileType { get; set; }
+}
